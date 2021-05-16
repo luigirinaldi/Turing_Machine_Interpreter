a36de739da68e6236fb911d818c710c36ee0f6ad
luigirinaldi <luigirinaldi06@gmail.com>
2021-05-16 10:58:59 +0200

    working, well done dynamic string arrays with calloc

--- Test1/inputTest.c
+++ Test1/inputTest.c
@@ -1,30 +1,31 @@
 #include <stdio.h> 
+#include <stdlib.h> //contains memalloc and other stuff
 
 int main(){
 	int return_char = 10; //ascii value of \n 
 	int space_char = 32;  //ascii value of whitespace
 	
-	char string[1];
 	int string_size = 0;
-	char* string_pointer = string;
+	char* string_pointer;
+	string_pointer = calloc(string_size+1,sizeof(char)); // "create" array of chars
 
 	int in_char;
 	in_char = getchar(); //read first char
 	while(in_char != return_char){ //stop with line break
 		*(string_pointer+(string_size)) = in_char;	//assign current char to current string (char array pointed to by string_pointer)
 		string_size++;
-    	char new_stringArr[string_size+1];
-		for (int i =0;i<string_size;i++){
-			//copy values from previous array to new array
-			new_stringArr[i] = string_pointer[i];
+    	char* new_stringArr;
+		new_stringArr = calloc(string_size+1,sizeof(char)); //create new array with correct size
+		for (int i =0;i<string_size;i++){			
+			*(new_stringArr+i) = *(string_pointer+i);		//copy values from previous array to new array
 		}
-		string_pointer[string_size] = 0;
-		//update pointer to point to new array
-		string_pointer = &new_stringArr[0];	
+		*(new_stringArr+string_size) = 0;	//initialize spot for next value (probably redundant)
+		free(string_pointer); 				//delete previously created array		
+		string_pointer = new_stringArr;		//update pointer to point to new array
 		in_char = getchar();
 	}
 
-	int spaces[string_size];
+	int* spaces = calloc(string_size,sizeof(int));
 	int num_strings = 1;
 
 	printf("there are %d chars in the string\n",string_size);
@@ -32,7 +33,6 @@ int main(){
 
 	//loop to print chars and find blank spaces(ascii 32)
 	for(int i = 0;i<string_size;i++){
-		*(spaces+i) = 0;
 		//putchar(i+'0');
 		if(*(string_pointer+i) == space_char){
 			//found whitespace print new line char
@@ -45,33 +45,23 @@ int main(){
 		}
 	}
 	putchar('\n');
-	for(int i = 0;i<string_size;i++){
-		putchar(string_pointer[i]);
-	}
-	putchar('\n');
 
-/*
-    char* string_array[num_strings];
+	//CREATE STRING ARRAY
+    char** string_array = calloc(num_strings, sizeof(char));
 	if(num_strings > 1){
 		int prevpos = 0;
-		char* curr_string_pointer= string; // string is char array declared earlier
 		for(int i =0;i<num_strings;i++){
-			char curr_string[spaces[i]-prevpos];
-			curr_string_pointer = curr_string;
-			for (int j =prevpos; j<spaces[i];j++){
-				*(curr_string_pointer+(j-prevpos)) = &string_pointer[j];
+			char* curr_string = calloc(spaces[i]-prevpos,sizeof(char));		//assign memory space to string 
+			for (int j = prevpos; j<spaces[i];j++){
+				*(curr_string+(j-prevpos)) = *(string_pointer+j);
 			}			
-			string_array[i] = curr_string_pointer;
+			*(string_array+i) = curr_string;	//assign current string to ith position of char matrix/string array
 			prevpos = spaces[i];
 		}
 	}
-	printf("\n%d",string_array[0]);
-	printf("\n%c",string_array[0][0]);
-	printf("\n%d",string_array[0][1]);
-	printf("\n%d",string_array[1]);
-	printf("\n%d",string_array[1][1]);
-*/
-	printf("\nfinished listening to chars");
-    printf("\nthere are %d strings",num_strings);
+	printf("%s\n",string_array[0]);
+
+	printf("finished listening to chars \n");
+    printf("there are %d strings \n",num_strings);
 	return 0;
 }
