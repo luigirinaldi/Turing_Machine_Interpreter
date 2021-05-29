e074a40562b5fbd3113c8ac5df256c5a884a5ec5
luigirinaldi <luigirinaldi06@gmail.com>
2021-05-29 11:56:45 +0200

    starting to implement txt read in mdt

--- MDT_interpreter/funcs.h
+++ MDT_interpreter/funcs.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 
 void custom_delay(int time_in_milliseconds){
     double delay_time = (double)time_in_milliseconds/1000;
@@ -10,4 +11,194 @@ void custom_delay(int time_in_milliseconds){
     while(timediff<delay_time){
         timediff = ((double)clock()/CLOCKS_PER_SEC)-((double)curr_time/CLOCKS_PER_SEC);
     }  
+}
+
+int find_index(char *state,char **states,int num_states){
+    for(int i = 0;i < num_states;i++){
+        if(strcmp(state,states[i])==0){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int find_char(char needle,char *haystack,int size){
+    for(int i = 0;i < size;i++){
+        if(needle == haystack[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void read_mdt(char *abs_path,char ***trans_func,int *states_size,char **states_array,char *in_vals, int *alphabet_size,int *state_ins){
+    FILE *file;
+    file = fopen(abs_path,"r");
+    if(file == NULL){
+        printf("file read failed! \n");
+        return;
+    }
+    printf("successfully opened file!\n");
+    char quintupla = 0;
+    char virgola = 0;
+    int q_count = 0;
+
+    char quadruple[4] = {0,0,0,0};
+
+    
+
+    //variable length arrays    
+    int string_length = 0;
+    char *new_string = (char*) calloc(string_length+1,sizeof(char));
+    int num_states = 0;
+    
+    int num_ins = 0;
+    int curr_index = 0;
+
+    int prev_state_size = 0;
+    char **state_func = (char**) calloc(num_ins+1,sizeof(char*));
+
+    //parameter initialization
+    trans_func = (char***) calloc(num_states+1,sizeof(char**));
+    states_array = (char**) calloc(num_states+1,sizeof(char*));
+    in_vals = (char*) calloc(num_ins+1,sizeof(char));    
+    state_ins = (int*) calloc(1,sizeof(int));  
+    
+
+    /*char trans_func[500][500][4];
+    int state_ins[500] = {0};*/
+
+    char new_char;
+    new_char = fgetc(file);
+    while(!feof(file)){ //temporarily i, should be !feof(file)
+        if(new_char == ','){
+            virgola = 1;
+        } else if (new_char == '(' && quintupla == 0){
+            quintupla = 1;
+            //reset vals
+            for(int i = 0;i<4;i++){
+                quadruple[i] = 0;
+            }
+        } else if (new_char == ')' && quintupla == 1){      //finished reading quintuple, reset everything   
+            /*
+            resize state ins array
+            resize transfunc 
+            resize transfunc[curr_index] to state_ins[curr_index size] with realloc
+            assign the new quadruple to transfunc[curr_index][state_ins[curr_index]] 
+            */         
+            
+            //resize state ins array
+            state_ins = (int*) realloc(state_ins,num_states*sizeof(int));           
+
+            //resize trans_func
+            trans_func = (char***) realloc(trans_func,num_states*sizeof(char**));
+
+            //initialize trans_func and state_ins
+            if(prev_state_size != num_states){
+                for(int i=prev_state_size;i<num_states;i++){
+                    state_ins[i] = 0;           //initialize new values to 0
+                    trans_func[i] = (char**) calloc(1,sizeof(char*)); //initialize state array containing quadruples
+                }
+            }
+            prev_state_size = num_states;
+
+            trans_func[curr_index] = (char**) realloc(trans_func[curr_index],(state_ins[curr_index]+1)*sizeof(char*));           
+            
+            int counter = state_ins[curr_index];
+            trans_func[curr_index][counter] = (char*) calloc(4,sizeof(char));
+            trans_func[curr_index][counter][0] = quadruple[0];
+            trans_func[curr_index][counter][1] = quadruple[1];
+            trans_func[curr_index][counter][2] = quadruple[2];
+            trans_func[curr_index][counter][3] = quadruple[3];
+            state_ins[curr_index]++;
+            
+            q_count = 0;
+            quintupla = 0;
+        } else if (quintupla == 1 && q_count < 5){
+            if(virgola == 1){
+                virgola = 0;
+                if(q_count == 0){
+                    //this means state has stopped being read, and it can be added to state array
+                    curr_index = find_index(new_string,states_array,num_states);
+                    if(curr_index == -1){
+                        num_states++;
+                        states_array = (char**) realloc(states_array,num_states*sizeof(char*));
+                        states_array[num_states-1] = new_string;
+                        curr_index = num_states-1;
+                    }
+
+                    string_length = 0;
+                    new_string  = (char*) calloc(string_length+1,sizeof(char));                    
+                    new_string[0] = '\0';                
+                } else if (q_count == 2){
+                    //finished reading the outgoing state
+                    int out_index = find_index(new_string,states_array,num_states);
+                    if(out_index == -1){
+                        num_states++;
+                        states_array = (char**) realloc(states_array,num_states*sizeof(char*));
+                        states_array[num_states-1] = new_string;
+                        out_index = num_states-1;
+                    }
+                    quadruple[2] = out_index;
+
+                    string_length = 0;
+                    new_string  = (char*) calloc(string_length+1,sizeof(char));                    
+                    new_string[0] = '\0';       
+                }     
+                q_count++;
+            }
+            if(q_count == 0){ 
+                //contatore 0, quindi sto leggendo lo stato
+                new_string[string_length] = new_char;
+                string_length++;
+                new_string = (char*) realloc(new_string,(string_length+1)*sizeof(char));  //resize string array to fit next value
+                new_string[string_length] ='\0';      //adding terminator char to end string
+            } else if (q_count ==1){
+                //reading first input 
+                quadruple[0] = new_char;
+
+                int in_pos = find_char(new_char,in_vals,num_ins);
+                if(in_pos == -1){
+                    num_ins++;
+                    in_vals = (char*) realloc(in_vals,num_ins*sizeof(char));
+                    in_vals[num_ins-1] = new_char;
+                    in_pos = num_ins -1;
+                }
+            } else if (q_count == 2){
+                //output state 
+                new_string[string_length] = new_char;
+                string_length++;
+                new_string = (char*) realloc(new_string,(string_length+1)*sizeof(char));  //resize string array to fit next value
+                new_string[string_length] ='\0';      //adding terminator char to end string
+            } else if (q_count == 3){
+                //reading output
+                quadruple[1] = new_char;
+
+                int in_pos = find_char(new_char,in_vals,num_ins);
+                if(in_pos == -1){
+                    num_ins++;
+                    in_vals = (char*) realloc(in_vals,num_ins*sizeof(char));
+                    in_vals[num_ins-1] = new_char;
+                    in_pos = num_ins -1;
+                }
+            } else if (q_count == 4){
+                //direction char
+                quadruple[3] = new_char;
+            }
+        }
+        new_char = fgetc(file);
+    }
+    printf("{");
+    for(int i=0;i<num_states;i++){
+        int j = 0;
+        printf(" { %s:\n",states_array[i]);
+        while(j<state_ins[i]){
+            printf("    {%c,%c,%s,%c}\n",trans_func[i][j][0],trans_func[i][j][1],states_array[trans_func[i][j][2]],trans_func[i][j][3]);
+            j++;
+        }        
+        printf(" }");
+    }
+    printf("}\n");
+    (*alphabet_size) = num_ins;
+    (*states_size) = num_states;
 }
---
+++ MDT_interpreter/mdt_read.c
@@ -0,0 +1,246 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "funcs.h"          //custom include, to not have functions bloating code
+
+#ifdef _WIN32
+    #include <windows.h> 
+#endif 
+
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#define RESET_STYLE "\x1b[0m"
+#define DEFAULT_COLOR "\x1b[37m"                //white text
+#define HIGHLIGHT_TEXT "\x1b[31m\x1b[1m"       //bold and red
+
+
+char input_string[] = "-AABBABBABBA-";
+int DELAY_TIME = 500; //100 ms
+
+int main(){
+    #ifdef _WIN32
+	    HANDLE hStdout;
+    	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    	DWORD mode, originalMode;
+    	GetConsoleMode(hStdout,&mode);
+    	originalMode = mode;
+    	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    	SetConsoleMode(hStdout, mode);    
+    #endif    
+
+    //options stuff
+    char trailing_lambdas = 0;
+    printf("insert delay\n");
+    scanf(" %d",&DELAY_TIME);
+    printf("custom options? (y/n)\n");
+    char custom_options;
+    scanf(" %c",&custom_options);
+    if(custom_options == 'y'){
+        
+        printf("remove trailing lambas? (y/n)\n");
+        scanf(" %c",&trailing_lambdas);
+        if(trailing_lambdas == 'y'){
+            trailing_lambdas = 1;
+        } else{
+            trailing_lambdas = 0;
+        }
+        printf("custom input string? (y/n)\n");
+        char custom_input;
+        scanf(" %c",&custom_input);
+        if(custom_input == 'y'){
+            printf("insert new string:\n");
+            scanf(" %s",input_string);
+        }
+    }
+    printf("this is where the fun begins\n");
+    custom_delay(DELAY_TIME);
+
+    char *filename = "bruh.txt";
+
+    char *buf = getcwd(NULL, 0); //automagically assigns intself correctly sized buffer for current working directory(cwd)
+    if (buf == NULL) {
+        printf("failed while retrieving current dir\n");
+    }
+    printf("Current working directory: %s\n", buf);
+    char abs_path[sizeof(buf)+sizeof(filename)+2];
+    sprintf(abs_path,"%s\\%s",buf,filename); //join cwd and filename to get absolute path of txt file
+    
+    char ***trans_func;
+    int num_states;
+    char **states;
+    char *alphabet;
+    int alphabet_size;
+    int *state_in_sizes;
+
+    read_mdt(
+        abs_path, 
+        trans_func,
+        &num_states,
+        states,
+        alphabet,
+        &alphabet_size,
+        state_in_sizes
+    );
+
+    printf("num states: %d\n",num_states);
+    int initial_state = 0;
+    int final_state;
+    for(int i =0;i<num_states;i++){
+        printf("%d, %d",i,state_in_sizes[i]);
+        if(state_in_sizes[i] ==0){
+
+            final_state = i;
+            printf("final state is %d\n",final_state);
+        }
+    }
+    int cursor_pos = 1;                                     //start on first valid letter
+    int prev_cursor_pos = cursor_pos;
+    int curr_state = initial_state;                         //start with first state
+    char reprint_tape = 0;                                  //char used as boolean to check whether or not stirng has to be reprinted
+
+
+    int tape_len = strlen(input_string);
+    char *tape = (char*) calloc(tape_len,sizeof(char));     //assign memory for tape 
+    memcpy(tape,input_string,tape_len);
+
+    
+    
+    printf("\x1b[1;1H\x1b[2J");                             //clear canvas and set cursor to 1,1 and
+    printf("\x1b[?25l");                                    //make cursor invisible 
+    printf(RESET_STYLE);                                    //reset all styles 
+    printf(DEFAULT_COLOR);                                  //color with code 37 (white)
+    printf("%s",tape);                              //print first time      
+    printf("\x1b[2;1f");				                    //position cursor to print state
+    char initial_string[] = "curr state: ";       
+    printf("%s%s",initial_string,states[curr_state]);  
+    custom_delay(DELAY_TIME);         
+    while(curr_state != final_state){        
+        char curr_char = tape[cursor_pos];
+        int curr_symbol;
+        char found = 0;
+        
+        for(int i = 0; i < state_in_sizes[i] && found != 1;i++){                //find index of current input, by comparing first value of innermost array
+            if(trans_func[curr_state][i][0]==curr_char){
+                curr_symbol = i;
+                found = 1;              
+            }
+        }
+
+        if (found == 1){
+            char next_char = trans_func[curr_state][curr_symbol][1];
+            int next_state = trans_func[curr_state][curr_symbol][2];
+            char direction = trans_func[curr_state][curr_symbol][3];    
+
+            tape[cursor_pos] = next_char;               //update input string 
+            curr_state = next_state;                            //update state 
+                                
+            
+            //reprint old value 
+            printf("\n");				                    //adding so that it works on ipad
+            printf("\x1b[1;%df",prev_cursor_pos+1);       	//move cursor to previous char 
+            printf("%s%s",RESET_STYLE,DEFAULT_COLOR);		//reset style of previous char
+            putchar(tape[prev_cursor_pos]);
+
+            //print new value 
+            printf("\x1b[1;%df",cursor_pos+1);              //move cursor to cursor pos to update string, +1 because terminal coords start from 1,1
+            printf(HIGHLIGHT_TEXT);                         //highlight text property defined at the start
+            putchar(next_char);
+            //reset style for cells after the current one
+            printf("\x1b[1;%df",cursor_pos+2);	            //move to next cell
+            printf("%s%s",RESET_STYLE,DEFAULT_COLOR);       //reset style for cells on forward
+                
+            //update current state
+            putchar('\n'); //again added so ipad works kinda
+            printf("\x1b[2;%df",strlen(initial_string)+1);                    //move to update current state
+            printf("\x1b[0J");                                                //clear from cursor to end of line
+            printf("%s",states[curr_state]);
+
+            printf(RESET_STYLE);
+                
+            prev_cursor_pos = cursor_pos;                   //save old position to reset it to normal text
+            switch(direction){                              //update cursor pos based on direction        
+                case '>':
+                    cursor_pos++;
+                    break;
+                case '<':
+                    cursor_pos--;
+                    break;
+                case '-':
+                    //cursor doesn't move
+                    break;
+            }
+
+            if(trailing_lambdas){
+                int found_char = 1;
+                int counter = 1;
+                while(found_char){
+                    if(tape[counter] == '-'){
+                        tape_len--;
+                        char *new_tape = (char*) calloc(tape_len,sizeof(char));
+                        memcpy(new_tape,tape+1,tape_len);         //shift all values of tape to the left, because first one is '-'
+                        free(tape);
+                        tape = new_tape;
+                        cursor_pos--;
+                        prev_cursor_pos--;
+                        reprint_tape = 1;
+                    } else{
+                        found_char = 0;
+                    }
+                }
+            }
+
+            if(cursor_pos < 1){                             //resize tape array
+                tape_len++;
+                char *new_tape = (char*) calloc(tape_len,sizeof(char));
+                memcpy(new_tape+1,tape,tape_len-1);         //copy value from old tape to new one, starting from one posisiton ahead
+                new_tape[0]='-';
+                free(tape);
+                tape = new_tape;
+                cursor_pos++;
+                prev_cursor_pos++;
+                reprint_tape = 1;
+            } else if(cursor_pos > (tape_len-2)){           //if it is greater than the last '-' then increase the array
+                tape_len++;
+                char *new_tape = (char*) calloc(tape_len+1,sizeof(char));
+                memcpy(new_tape,tape,tape_len-1);         //copy value from old tape to new one, starting from one posisiton ahead
+                new_tape[tape_len-1]='-';
+                new_tape[tape_len] = '\0';
+                free(tape);
+                tape = new_tape;
+                reprint_tape = 1;
+            }
+
+            if(reprint_tape){
+                printf("\n");
+                printf("\x1b[1;1f");                        //move to 1,1
+                printf("%s%s",RESET_STYLE,DEFAULT_COLOR); 
+                printf("%s",tape);                          //print new tape
+                reprint_tape = 0;
+            }
+        } else{
+            putchar('\n'); //again added so ipad works kinda
+            printf("\x1b[2;%df",strlen(initial_string)+1);                    //move to update current state
+            printf("\x1b[0J");                       //clear from cursor to end of line
+            printf("%s","the mdt machine has failed :(");
+            printf(RESET_STYLE);
+            curr_state = final_state;
+        }
+        custom_delay(DELAY_TIME);
+    }
+
+    putchar('\n');
+    printf("\x1b[3;1f");                        //move to 3,1
+    printf("finished!\n");
+
+    printf("\x1b[?25h");                    //cursor is back to being visible
+    printf(RESET_STYLE);
+    #ifdef _WIN32
+    	SetConsoleMode(hStdout,originalMode);
+    #endif
+    return 0;
+}
+
+
